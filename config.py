744a5f91c391571315273835b2e260169a004712
Fatma Fauziah <fatmafauziahpam@gmail.com>
2026-07-17 12:57:14 +0700

    support PostgreSQL

--- config.py
+++ config.py
@@ -3,10 +3,12 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "capstone-secret-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "capstone-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = \
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
         "sqlite:///" + os.path.join(BASE_DIR, "database.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
