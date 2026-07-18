fb3342f9a39e7cd463c588569c5d379f37c18a3a
Fatma Fauziah <fatmafauziahpam@gmail.com>
2026-07-18 08:17:55 +0700

    Fix config indentation

--- config.py
+++ config.py
@@ -7,12 +7,16 @@ class Config:
 
     database_url = os.environ.get("DATABASE_URL")
 
-if database_url and database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
 
-SQLALCHEMY_DATABASE_URI = database_url or (
-    "sqlite:///" + os.path.join(BASE_DIR, "database.db")
-)
+    SQLALCHEMY_DATABASE_URI = database_url or (
+        "sqlite:///" + os.path.join(BASE_DIR, "database.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
